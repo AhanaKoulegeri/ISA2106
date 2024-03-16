8557fe6e53e9c15af10d7021082b4b06a92fe8a9
Ahana Koulegeri <Ahana Koulegeri@LAPTOP-CK29C7IR>
2024-03-16 17:41:33 +0530

    Commit

---
+++ Java/src/Programs/ArmstrongNumber.java
@@ -0,0 +1,19 @@
+package Programs;
+
+public class ArmstrongNumber {
+	public boolean isArmstrong(int number) {
+        int originalNumber = number;
+        int numDigits = String.valueOf(number).length();
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, numDigits);
+            number /= 10;
+        }
+
+        return originalNumber == sum;
+    }
+}
+
+
