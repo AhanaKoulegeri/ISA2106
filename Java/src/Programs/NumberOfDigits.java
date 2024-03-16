93c784118bd447b695fc14ba7b4a6cade690248d
Ahana Koulegeri <Ahana Koulegeri@LAPTOP-CK29C7IR>
2024-03-16 17:27:33 +0530

    Commit

---
+++ Java/src/Programs/NumberOfDigits.java
@@ -0,0 +1,17 @@
+package Programs;
+
+public class NumberOfDigits {
+	 public int countDigits(int number) {
+	        if (number == 0)
+	            return 1;
+	        
+	        int count = 0;
+	        while (number != 0) {
+	            number /= 10;
+	            count++;
+	        }
+	        return count;
+	    }
+	}
+
+
