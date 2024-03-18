b5161634db017a3ea942751f359c0c1f36218083
AhanaKoulegeri <154397738+AhanaKoulegeri@users.noreply.github.com>
2024-03-18 11:46:45 +0530

    Update ArmstrongNumberTest.java changes

--- Java/src/Programs/ArmstrongNumberTest.java
+++ Java/src/Programs/ArmstrongNumberTest.java
@@ -3,7 +3,7 @@ package Programs;
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.AfterClass;
+import org.junit.AfterClasss;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
